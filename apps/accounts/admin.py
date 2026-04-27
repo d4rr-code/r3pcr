23274011009761722282067ff8c44b5723e3dc7c
Francis Dar <darfrancis33@gmail.com>
2026-04-27 08:21:28 +0800

    end of session - apr 26

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, OTP
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'role', 'is_active']
+    list_filter = ['role', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        ('R3-PCR Info', {'fields': ('role', 'phone_number')}),
+    )
+
+@admin.register(OTP)
+class OTPAdmin(admin.ModelAdmin):
+    list_display = ['user', 'code', 'is_used', 'created_at']
--- apps/shipments/admin.py
+++ apps/shipments/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Shipment, ShipmentDocument, HSCode, ShipmentHSCode, StatusLog
 
-# Register your models here.
+@admin.register(HSCode)
+class HSCodeAdmin(admin.ModelAdmin):
+    list_display = ['code', 'description', 'duty_rate', 'unit', 'chapter', 'is_active']
+    search_fields = ['code', 'description']
+    list_filter = ['is_active', 'chapter']
+    list_editable = ['duty_rate', 'is_active']
+
+@admin.register(Shipment)
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = ['hawb_number', 'consignee', 'declarant', 'status', 'submitted_at']
+    search_fields = ['hawb_number']
+    list_filter = ['status', 'urgency']
+
+@admin.register(ShipmentDocument)
+class ShipmentDocumentAdmin(admin.ModelAdmin):
+    list_display = ['shipment', 'document_type', 'uploaded_at']
+
+@admin.register(StatusLog)
+class StatusLogAdmin(admin.ModelAdmin):
+    list_display = ['shipment', 'old_status', 'new_status', 'changed_by', 'changed_at']
