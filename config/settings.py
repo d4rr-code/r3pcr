5a5ad9a0dcffcae68ae9627068161b767dd8b75b
Francis Dar <darfrancis33@gmail.com>
2026-05-13 16:08:51 +0800

    fix: add CSRF_TRUSTED_ORIGINS from env var

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,8 @@ DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:8000').split(',')
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
