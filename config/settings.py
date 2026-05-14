cb0902dc7be31e755cb855861c62be6159503fba
Francis Dar <darfrancis33@gmail.com>
2026-05-14 13:19:18 +0800

    fix: use Supabase public URL for document links (not S3 endpoint)

--- config/settings.py
+++ config/settings.py
@@ -130,15 +130,19 @@ _SUPABASE_BUCKET = os.getenv('SUPABASE_BUCKET', 'r3pcr-media')
 if _SUPABASE_URL and _SUPABASE_KEY:
     STORAGES['default']['BACKEND'] = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    AWS_ACCESS_KEY_ID     = os.getenv('SUPABASE_S3_ACCESS_KEY_ID', '')
-    AWS_SECRET_ACCESS_KEY = os.getenv('SUPABASE_S3_SECRET_ACCESS_KEY', '')
+    _project_id = _SUPABASE_URL.replace('https://', '').split('.')[0]
+
+    AWS_ACCESS_KEY_ID       = os.getenv('SUPABASE_S3_ACCESS_KEY_ID', '')
+    AWS_SECRET_ACCESS_KEY   = os.getenv('SUPABASE_S3_SECRET_ACCESS_KEY', '')
     AWS_STORAGE_BUCKET_NAME = _SUPABASE_BUCKET
-    AWS_S3_ENDPOINT_URL   = f'{_SUPABASE_URL}/storage/v1/s3'
-    AWS_S3_REGION_NAME    = 'ap-southeast-1'
-    AWS_DEFAULT_ACL       = 'public-read'
-    AWS_S3_FILE_OVERWRITE = False
-    AWS_QUERYSTRING_AUTH  = False                # public URLs, no signed expiry
-    MEDIA_URL = f'{_SUPABASE_URL}/storage/v1/object/public/{_SUPABASE_BUCKET}/'
+    AWS_S3_ENDPOINT_URL     = f'{_SUPABASE_URL}/storage/v1/s3'
+    AWS_S3_REGION_NAME      = 'ap-southeast-1'
+    AWS_DEFAULT_ACL         = 'public-read'
+    AWS_S3_FILE_OVERWRITE   = False
+    AWS_QUERYSTRING_AUTH    = False
+    # Generate public URLs via Supabase's public endpoint (not the S3 endpoint)
+    AWS_S3_CUSTOM_DOMAIN    = f'{_project_id}.supabase.co/storage/v1/object/public/{_SUPABASE_BUCKET}'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 # Auth
 LOGIN_URL = '/accounts/login/'
